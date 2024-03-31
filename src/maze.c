74ab174c78b7155981ebba5c78d3ec91f8578cad
huldades <huldades@student.21-school.ru>
2024-03-31 23:19:20 +0300

    develop: bug fix

--- src/maze.c
+++ src/maze.c
@@ -91,7 +91,7 @@ int main() {
     }
 
     if (y < MAZE_Y - 1) {
-      for (int x = 0; x < MAZE_X - 1; x++) {
+      for (int x = 0; x < MAZE_X; x++) {
         if (maze.horizontal[y][x] == 0) {
           maze.group[y + 1][x] = maze.group[y][x];
         }
@@ -100,9 +100,9 @@ int main() {
     } else {
       for (int x = 0; x < MAZE_X - 1; x++) {
         if (maze.group[y][x] != maze.group[y][x + 1]) {
-          maze.horizontal[y][x] = 0;
+          maze.vertical[y][x] = 0;
 
-          int group_to_swap = maze.group[y + 1][x];
+          int group_to_swap = maze.group[y][x + 1];
           int target = maze.group[y][x];
 
           for (int i = x; i < MAZE_X; i++) {
