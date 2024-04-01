822d6116fa06ff989fae7da129445fb576a1f31a
huldades <huldades@student.21-school.ru>
2024-04-01 14:40:50 +0300

    develop: убрал везде зависимость от define костылём ☺

--- src/maze.h
+++ src/maze.h
@@ -6,13 +6,13 @@
 #include <sys/time.h>
 #include <time.h>
 
-#define MAZE_X 10
-#define MAZE_Y 10
+#define MAZE_X 10  // delete me
+#define MAZE_Y 10  // delete me
 
 typedef struct Maze_struct {
-  int vertical[MAZE_Y + 1][MAZE_X + 1];
-  int horizontal[MAZE_Y + 1][MAZE_X + 1];
-  int map[MAZE_Y * 2 + 1][MAZE_X * 2 + 1];
+  int vertical[51][51];
+  int horizontal[51][51];
+  int map[101][101];
   int x;
   int y;
 } Maze;
@@ -21,6 +21,5 @@ void read_from_file(Maze* test, char* filename);
 void draw(Maze* test);
 void get_map(Maze* test);
 void print_map(Maze maze);
-
 
 #endif
