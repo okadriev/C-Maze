2b7da608788094f72daa22d327b1d7ade80c4bbd
Ekaterina <sema190694@mail.ru>
2024-04-03 15:59:31 +0300

    hsiucard: fix grot + tests

---
+++ src/grot.h
@@ -0,0 +1,34 @@
+#ifndef GROT_H
+#define GROT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MAZE_X 50
+#define MAZE_Y 50
+#define MIN_LIMIT 0
+#define MAX_LIMIT 7
+#define MIN_CHANCE 1
+#define MAX_CHANCE 10
+
+typedef struct Grot_struct {
+  int grotto[MAZE_Y][MAZE_X];
+  int birth;
+  int death;
+  int chance;
+} Grot;
+
+void grot();
+void generate_grot(Grot* grot);
+void life_grot(Grot* grot);
+int count_life(Grot* grot, int i, int j);
+void user_input(Grot* grot);
+int choose_mode(int* n);
+void print_step_by_step(Grot* grot);
+void print_auto(Grot* grot, int n);
+void print_grot(Grot* grot);
+int compare(int grotto[MAZE_Y][MAZE_X]);
+
+#endif
